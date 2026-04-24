460a0f7dc1e4a9eb54ddea0db32db476d6e18bfe
techwithshivankssharma <shivankjaiwal5555@gmail.com>
2026-04-24 17:14:51 +0530

    24/04/26

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users",require("./routes/userRoutes"));
 
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
 const PORT = process.env.PORT || 5000;  
 
 app.listen(PORT,() => {
